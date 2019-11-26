42dbf34492db42ccef0c1976a21a3d1b16af0f62
Alex Kayser <akctba@gmail.com>
2019-11-26 08:30:02 -0800

    State verifications

--- HiParcel/src/ca/ciccc/ak/hiparcel/model/State.java
+++ HiParcel/src/ca/ciccc/ak/hiparcel/model/State.java
@@ -6,4 +6,46 @@ public enum State {
 	Transit, //parcel with courier
 	Delivered, //parcel with recipient
 	Returned; //percel returned to sender
+	
+	/**
+	 * Return the default initial state
+	 * @return
+	 */
+	public static State initial() {
+		return State.Draft;
+	}
+	
+	/**
+	 * Verify if the service order is in a finished state
+	 * @return
+	 */
+	public boolean finished() {
+		boolean f = false;
+		if(this == State.Delivered || this == State.Returned) {
+			f = true;
+		} 
+		return f;
+	}
+	
+	/**
+	 * Return the next state to service order
+	 * @return
+	 */
+	public State next() {
+		State next = null;
+		switch (this) {
+		case Draft:
+			next = State.Ready;
+			break;
+		case Ready:
+			next = State.Transit;
+			break;
+		case Transit:
+			next = State.Delivered;
+			break;
+		default:
+			next = this;
+		}
+		return next;
+	}
 }
