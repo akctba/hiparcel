6fa240c38c77dd3d455dafd02cf625129c5f9598
Alex Kayser <akctba@gmail.com>
2019-11-27 20:18:32 -0800

    comments

--- HiParcel/src/ca/ciccc/ak/hiparcel/model/User.java
+++ HiParcel/src/ca/ciccc/ak/hiparcel/model/User.java
@@ -1,10 +1,25 @@
 package ca.ciccc.ak.hiparcel.model;
 
+/**
+ * This model class represents Sender and Recipient
+ * @author alexkayser
+ *
+ */
 public class User {
 	
 	private long ID;
 	
 	private String name;
+	
+	public User() {
+		//default constructor
+	}
+
+	public User(long iD, String name) {
+		super();
+		ID = iD;
+		this.name = name;
+	}
 
 	public long getID() {
 		return ID;
