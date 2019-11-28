8c7ad0b401a6bfdc1a18224dc9a4d3f53e40c98c
Alex Kayser <akctba@gmail.com>
2019-11-27 20:18:17 -0800

    Class to represents the database

---
+++ HiParcel/src/ca/ciccc/ak/hiparcel/adb/Users.java
@@ -0,0 +1,33 @@
+package ca.ciccc.ak.hiparcel.adb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.ciccc.ak.hiparcel.model.User;
+
+/**
+ * Class to access database
+ * @author alexkayser
+ *
+ */
+public class Users {
+
+	// temporary constants. It will be changed to database access
+	private static final String[] names = { "Edgar Gray", "Randal Nichols", "Howard Waters", "Perry Cain",
+			"Felipe Rose", "Gilberto Obrien", "Cindy Sparks", "Lorena Weber", "Pam Anderson", "Jan Rios" };
+
+	public List<User> allCurrentUsers() {
+		List<User> list = new ArrayList<User>();
+		int i = 1;
+
+		//temporary code. Change to database access
+		for(String n : names) {
+			list.add(new User(i, n));
+			i++;
+		}
+		
+		return list;
+
+	}
+
+}
